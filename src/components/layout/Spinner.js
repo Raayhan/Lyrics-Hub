884e76524b027efde5e118d28bb3cd9931cfe676
1410400 <rakib.nsu.cse@gmail.com>
2020-05-18 01:34:45 +0600

    Added Top 10 songs list from Calling API

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import spinner from './spinner.gif';
 
 export default () => {
-    return (
-        <div>
-            <img 
-              src={spinner}
-              alt="Loading..."
-              style={{width: '200px', margin: ' 40px auto', dsiplay:'block'}}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <img
+        src={spinner}
+        alt="Loading..."
+        style={{ width: '200px', margin: ' 40px auto', display: 'block' }}
+      />
+    </div>
+  );
 };
