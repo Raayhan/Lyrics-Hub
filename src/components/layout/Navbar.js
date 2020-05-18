385678925fb680368d4dd41081d0a7c58105e405
1410400 <rakib.nsu.cse@gmail.com>
2020-05-18 18:15:21 +0600

    Added Logo and Favicon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark mb-5">
-          <span className="navbar-brand mb-0 h1 mx-auto">Lyrics Hub</span>  
+         
+          <span className="navbar-brand mb-0 h1 mx-auto"><img src="logo.png" height="80px" alt="Lyrics Hub"></img></span>  
          
         </nav>
     )
