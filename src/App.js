c7fa930b5f59c373b35f94ee7eef1dde67cc359a
1410400 <rakib.nsu.cse@gmail.com>
2020-05-21 00:35:55 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
   return (
     <Provider>
-        <Router basename={`${process.env.PUBLIC_URL}/`}>
+        <Router basename="/">
             <React.Fragment>
               <Navbar />
               
